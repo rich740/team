e2c3c68a93c8b5887f20e767b859c404cc5a058d
rich740 <richa.kumari@actiknow.com>
2025-03-31 10:17:39 +0530

    add services

--- src/services/employees.js
+++ src/services/employees.js
@@ -14,7 +14,10 @@ export const createEmployees = (values) => {
    return axios.delete(`${Endpoint.deleteEmployees}/${employeeId}`);
 }
 
-export const updateEmployees = (employeeId, teamId) => {
-   return axios.put(`/updateemployees/${employeeId}`, { teamId });
-   // This URL might be incorrect
+// export const updateEmployees = (employeeId, teamId) => {
+//    return axios.put(`/updateemployees/${employeeId}`, { teamId });
+//    // This URL might be incorrect
+//  }
+ export const updateEmployees = (employeeId, teamId) => {
+   return axios.put(`${Endpoint.updateEmployees}/${employeeId}`, { teamId });
  }
